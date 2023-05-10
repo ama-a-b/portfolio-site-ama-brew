732eb41d7f7b345294a9db738e92fc626f302931
ama-a-b <brewama16@gmail.com>
2023-05-10 14:06:17 +0100

    Menu added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log(Hi World! Welcome to My Portfolio Site. Take a look around, I hope you enjoy my Worker!)
+console.log("Hi World! Welcome to My Portfolio Site. Take a look around, I hope you enjoy my Worker!")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
